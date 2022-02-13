2d9ec9b3ed93a7239a735f482b51d464f64347be
Lorena Castro <lcastro0398@gmail.com>
2022-02-12 23:43:58 -0500

    Se resuelve la segunda actividad

---
+++ Exercises/src/exercise2/Main3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercise2;
+
+/**
+ *
+ * @author lcast
+ */
+public class Main3 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Definitivamente sí se puede");
+    }
+}
