10c788df8a7bac0812f1215e9c3902ff65df4ed4
Lorena Castro <lcastro0398@gmail.com>
2022-02-12 23:08:41 -0500

    Ya es posible calcular la atracción gravitacional

--- Exercises/src/exercise1/planet.java
+++ Exercises/src/exercise1/planet.java
@@ -4,19 +4,20 @@
  */
 package exercise1;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.UUID;
 
 /**
  *
  * @author lcast
  */
-public class planet {
-    private UUID id;
+public class Planet {
+    private UUID id = UUID.randomUUID();
     private String name;
-    private float mass;
+    private BigDecimal mass;
     private float diameter;
-    private float distanceSun;
-    private final double G = 0.0000000000667408;
+    private BigDecimal distanceSun;
 
     public UUID getId() {
         return id;
@@ -30,11 +31,11 @@ public class planet {
         this.name = name;
     }
 
-    public float getMass() {
+    public BigDecimal getMass() {
         return mass;
     }
 
-    public void setMass(float mass) {
+    public void setMass(BigDecimal mass) {
         this.mass = mass;
     }
 
@@ -46,26 +47,44 @@ public class planet {
         this.diameter = diameter;
     }
 
-    public float getDistanceSun() {
+    public BigDecimal getDistanceSun() {
         return distanceSun;
     }
 
-    public void setDistanceSun(float distanceSun) {
+    public void setDistanceSun(BigDecimal distanceSun) {
         this.distanceSun = distanceSun;
     }
 
-    public double getG() {
-        return G;
-    }
-
-    public planet(UUID id, String name, float mass, float diameter, float distanceSun) {
-        this.id = id;
+    public Planet(String name, BigDecimal mass, float diameter, BigDecimal distanceSun) {
         this.name = name;
         this.mass = mass;
         this.diameter = diameter;
         this.distanceSun = distanceSun;
     }
-    
 
+    public static BigDecimal GravitationalAttraction(Planet planet1, Planet planet2){
+        BigDecimal G = new BigDecimal("6.67408E-11");
+        BigDecimal m1 = planet1.getMass();
+        BigDecimal m2 = planet2.getMass();
+        BigDecimal d = planet1.getDistanceSun().subtract(planet2.getDistanceSun());
+        
+        BigDecimal F = G.multiply((m1.multiply(m2))).divide(d.pow(2), 2, RoundingMode.HALF_EVEN);
+        return F;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Planet mercury = new Planet("Mercurio", new BigDecimal("3.302E23"), 4880, new BigDecimal("5.791E7"));
+        Planet venus = new Planet("Venus", new BigDecimal("4.867E24"), 12104, new BigDecimal("1.082E8"));
+        Planet earth = new Planet("Tierra", new BigDecimal("5.972E24"), 12756, new BigDecimal("1.496E8"));
+        Planet mars = new Planet("Marte", new BigDecimal("6.39E23"), 6794, new BigDecimal("2.2794E8"));
+        Planet jupiter = new Planet("Jupiter", new BigDecimal("1.899E27"), 142984, new BigDecimal("7.7833E8"));
+        Planet saturn = new Planet("Saturno", new BigDecimal("5.683E26"), 108728, new BigDecimal("1.4294E9"));
+        Planet uranus = new Planet("Urano", new BigDecimal("8.681E25"), 51118, new BigDecimal("2.87099E9"));
+        Planet neptune = new Planet("Neptuno", new BigDecimal("1.024E26"), 49532, new BigDecimal("4.5043E9"));
+        Planet pluto = new Planet("Plutón", new BigDecimal("1.29E22"), 2320, new BigDecimal("5.91352E9"));
+        
+        System.out.println("La atracción gravitacional de los dos planetas es: "+GravitationalAttraction(mercury, venus)+ "N");
+    }
     
 }
