1be90c04bbb43a58d6f4cbee4807d7ad4e88acb0
Lorena Castro <lcastro0398@gmail.com>
2022-02-14 01:26:15 -0500

    Se realiza la documentación del ejercicio 4.

--- Exercises/src/exercise4/Main.java
+++ Exercises/src/exercise4/Main.java
@@ -9,15 +9,18 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 /**
- *
- * @author lcast
+ * La clase Main contiene el método para iniciar el programa.
+ * 
+ * @version 1.00.000 2022-02-14, la clase corresponde a la versión 1 del sistema.
+ * 
+ * @author Lorena Castro - Lcastro0398@gmail.com
  */
 public class Main {
     /**
+     * Permite la interacción con el usuario para que este agregue las características de los vehículos deseados.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         ArrayList<Vehicle> VehicleList = new ArrayList<>();
         int add = 0;
         System.out.println("Bienvenido al sistema de control del Ferry. Por favor indique los datos de los vehiculos a transportar");
--- Exercises/src/exercise4/Vehicle.java
+++ Exercises/src/exercise4/Vehicle.java
@@ -9,8 +9,11 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
- *
- * @author lcast
+ * La clase Vehicle define los atributos (caracteristicas) que comparten los diferentes tipos de vehiculo.
+ * 
+ * @version 1.00.000 2022-02-14, la clase corresponde a la versión 1 del sistema.
+ * 
+ * @author Lorena Castro - Lcastro0398@gmail.com
  */
 public abstract class Vehicle {
     private String plate;
@@ -20,9 +23,23 @@ public abstract class Vehicle {
     private int wheels;
     private String meansTransport;
     
+    
+    /**
+     * Constructor vacío
+     */
     public Vehicle(){
     }
     
+    /**
+     * Constructor: permite crear una instancia de la clase Vehicle de acuerdo a los parametros estipulados.
+     * 
+     * @param plate
+     * @param registrationDate
+     * @param passengers
+     * @param crew
+     * @param wheels
+     * @param meansTransport 
+     */
     public Vehicle(String plate, Calendar registrationDate, int passengers, boolean crew, int wheels, String meansTransport) {
         this.plate = plate;
         this.registrationDate = registrationDate;
@@ -32,55 +49,106 @@ public abstract class Vehicle {
         this.meansTransport = meansTransport;
     }
 
+    /**
+     * Método accesor del atributo plate (placa del vehículo).
+     * @return 
+     */
     public String getPlate() {
         return plate;
     }
 
+    /**
+     * Método modificador del atributo plate.
+     * @param plate 
+     */
     public void setPlate(String plate) {
         this.plate = plate;
     }
 
+    /**
+     * Método accesor del atributo registrationDate (fecha de matriculación del vehículo).
+     * @return 
+     */
     public Calendar getRegistrationDate() {
         return registrationDate;
     }
 
+    /**
+     * Método modificador del atributo registrationDate.
+     * @param registrationDate 
+     */
     public void setRegistrationDate(Calendar registrationDate) {
         this.registrationDate = registrationDate;
     }
 
+    /**
+     * Método accesor del atributo passengers (pasajeros).
+     * @return 
+     */
     public int getPassengers() {
         return passengers;
     }
 
+    /**
+     * Método modificador del atributo passengers.
+     * @param passengers 
+     */
     public void setPassengers(int passengers) {
         this.passengers = passengers;
     }
 
+    /**
+     * Método accesor del atributo crew (presencia o ausencia de tripulación).
+     * @return 
+     */
     public boolean isCrew() {
         return crew;
     }
 
+    /**
+     * Método modificador del atributo crew.
+     * @param crew 
+     */
     public void setCrew(boolean crew) {
         this.crew = crew;
     }
 
+    /**
+     * Método accesor del atributo wheels (número de ruedas).
+     * @return 
+     */
     public int getWheels() {
         return wheels;
     }
 
+    /**
+     * Método modificador del atributo wheels.
+     * @param wheels 
+     */
     public void setWheels(int wheels) {
         this.wheels = wheels;
     }
 
+    /**
+     * Método accesor del atributo meansTransport (medio de desplazamiento).
+     * @return 
+     */
     public String getMeansTransport() {
         return meansTransport;
     }
 
+    /**
+     * Método modificador del atributp meansTransport
+     * @param meansTransport 
+     */
     public void setMeansTransport(String meansTransport) {
         this.meansTransport = meansTransport;
     }
 
-
+    /**
+     * Sobreescribe el método toString para mostrar los datos del vehículo de la manera deseada.
+     * @return 
+     */
     @Override
     public String toString() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");  
