3d3da18fd2138d9f89f4d6b05ba5f24e8332c059
Lorena Castro <lcastro0398@gmail.com>
2022-02-13 22:49:28 -0500

    Ya el Vector imprime los datos cuando se ingresa dos veces seguidas el mismo número.

--- Exercises/src/exercise6/Main.java
+++ Exercises/src/exercise6/Main.java
@@ -4,6 +4,9 @@
  */
 package exercise6;
 
+import java.util.Scanner;
+import java.util.Vector;
+
 /**
  *
  * @author lcast
@@ -14,5 +17,22 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        Vector<Integer> v = new Vector<>();
+        
+        int num = 0;
+        int lastItem = 0;
+        System.out.println("Por favor digite el número que desea agregar al array:");
+        Scanner s = new Scanner(System.in);
+        num = Integer.parseInt(s.nextLine());
+        v.addElement(num);
+
+        while (num != lastItem){
+            System.out.println("Por favor digite el número que desea agregar al array:");
+            num = Integer.parseInt(s.nextLine());
+            lastItem = v.lastElement();
+            v.addElement(num);
+        }
+        System.out.println(v);
+    } 
+             
 }
