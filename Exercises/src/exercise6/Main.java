ced8d5a850c9e94718b5689ebebbe4124ae805d7
Lorena Castro <lcastro0398@gmail.com>
2022-02-14 01:47:59 -0500

    Se realiza la documentación del ejercicio 6.

--- Exercises/src/exercise6/Main.java
+++ Exercises/src/exercise6/Main.java
@@ -8,30 +8,28 @@ import java.util.Scanner;
 import java.util.Vector;
 
 /**
- *
- * @author lcast
+ * La clase Main contiene el método para iniciar el programa.
+ * 
+ * @version 1.00.000 2022-02-14, la clase corresponde a la versión 1 del sistema.
+ * 
+ * @author Lorena Castro - Lcastro0398@gmail.com
  */
 public class Main {
     /**
+     * Permite la interacción con el usuario para que este escoja la forma de ordenar el vector.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Vector<Integer> v = new Vector<>();
         
         int num = 0;
-        int lastItem = 0;
-        System.out.println("Por favor digite el número que desea agregar al array:");
         Scanner s = new Scanner(System.in);
-        num = Integer.parseInt(s.nextLine());
-        v.addElement(num);
-
-        while (num != lastItem){
+        do {
             System.out.println("Por favor digite el número que desea agregar al array:");
             num = Integer.parseInt(s.nextLine());
-            lastItem = v.lastElement();
             v.addElement(num);
-        }
+            
+        } while ((v.size() == 1) || (num != v.elementAt(v.size()-2)));
         System.out.println(v);
     } 
              
